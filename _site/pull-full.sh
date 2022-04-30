2fca8ca7c18b1c2da615b3c9de0bc2fdc182378a
Robert Crooks <rcrooks@brightcove.com>
2022-04-30 11:21:19 -0400

    aapi data model

--- _site/pull-full.sh
+++
@@ -1,4 +0,0 @@
-git pull
-# pulled main repo
-git submodule update --remote
-# updated submodules
--- _site/push-full.sh
+++
@@ -1,7 +0,0 @@
-setopt localoptions rmstarsilent
-rm -f _site/feed.xml
-git pull
-./push-s.sh
-git add .
-git commit -m 'initial submodule testing - parent push'
-git push
